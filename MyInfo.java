892fbd156ba751acf353fdf4be644a5e2820324b
sandipmgr <gamingbunnysa@gmail.com>
2024-07-02 22:11:25 +0545

    lab2

---
+++ MyInfo.java
@@ -0,0 +1,13 @@
+public class MyInfo {
+    String name = "Deepal";
+    String phone = "9880881833";
+    String address ="Imadole";
+    public static void main(String[] args){
+        MyInfo studentObject = new MyInfo();
+        studentObject.name="Jolie";
+        studentObject.phone="987654321";
+        studentObject.address="Lalitpur";
+        System.out.println(studentObject.name+" "+studentObject.phone+" "+studentObject.address+"\n");
+    
+}
+}
---
+++ MyStudent.java
@@ -0,0 +1,17 @@
+public class MyStudent {
+    private String name;
+    private String phone;
+    private String address;
+    public static void main(String[] args){
+        MyStudent studentObject = new MyStudent();
+        System.out.println("First Output:");
+        System.out.println(studentObject.name+studentObject.phone+studentObject.address+"\n");
+        System.out.println("Second Output:");
+        System.out.println(studentObject.name+"\n"+studentObject.phone+"\n"+studentObject.address+"\n");
+        System.out.println("Third Output:");
+        System.out.println(studentObject.name+" "+studentObject.phone+" "+studentObject.address+"\n");
+
+    }
+}
+
+    
